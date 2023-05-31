413113dcd3b02a4adb144417c4b49adc693aa61d
Dzmitriyz <hekto2004@mail.ru>
2023-06-01 00:41:36 +0700

    Added tracking

--- app/src/main/java/com/example/odometer/MainActivity.java
+++ app/src/main/java/com/example/odometer/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.odometer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -16,7 +22,8 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     private OdometerService odometer;
     private boolean bound = false;
-
+    private  final int PERMISSION_REQUEST_CODE = 698;
+    private final int NOTIFICATION_ID = 423;
     private ServiceConnection connection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -60,9 +67,51 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart(){
         super.onStart();
+        if(ContextCompat.checkSelfPermission(this,OdometerService.PERMISSION_STRING)
+        != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{OdometerService.PERMISSION_STRING},
+                    PERMISSION_REQUEST_CODE);
+        }else{
+
         Intent intent = new Intent(this, OdometerService.class);
-        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);}
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(this, OdometerService.class);
+                    bindService(intent, connection, Context.BIND_AUTO_CREATE);
+                } else {
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                            .setSmallIcon(android.R.drawable.ic_menu_compass)
+                            .setContentTitle(getResources().getString(R.string.app_name))
+                            .setContentText(getResources().getString(R.string.permission_denied))
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .setVibrate(new long[] { 1000, 1000})
+                            .setAutoCancel(true);
+
+                    Intent actionIntent = new Intent(this, MainActivity.class);
+                    PendingIntent actionPendingIntent = PendingIntent.getActivity(this, 0,
+                            actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder.setContentIntent(actionPendingIntent);
+
+                    NotificationManager notificationManager =
+                            (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+
+                }
+            }
+        }
+    }
+
 
     @Override
     protected  void onStop(){
